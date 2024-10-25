12b9bc9d955f566b3990f9ef8f6a72d7ff9231fc
Jae-Hong-Kim <newfly101@naver.com>
2024-10-26 07:20:11 +0900

    [검색결과1] Store에서 모델값 설정 검색 되는지 확인

--- 1-vanilla/js/Store.js
+++ 1-vanilla/js/Store.js
@@ -6,5 +6,16 @@ export default class Store {
     if (!storage) throw "no storage";
 
     this.storage = storage;
+
+    // todo 검색 결과 초기화
+    this.searchKeyword = "";
+    this.searchResult = [];
+  }
+
+  // todo 검색 결과를 productData에 포함되어 있는지 체크하는 부분
+  search(keyword) {
+    this.searchKeyword = keyword;
+    this.searchResult = this.storage.productData.filter(product =>
+        product.name.includes(keyword));
   }
 }
