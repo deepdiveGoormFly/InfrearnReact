74b09304c4400ee5887fc1930b124077279c4ea1
Jae-Hong-Kim <newfly101@naver.com>
2024-10-26 07:21:18 +0900

    [검색결과1] view 추가하고 controller 연동하기

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,11 +1,12 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, {searchFormView}) {
+  constructor(store, {searchFormView, searchResultView}) {
     console.log(tag);
     this.store = store;
 
     this.searchFormView = searchFormView;
+    this.searchResultView = searchResultView;
 
     this.subscribeViewEvents();
   }
