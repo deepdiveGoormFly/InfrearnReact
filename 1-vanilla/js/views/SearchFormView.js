213b7400ca5760b0764fc11e4141216ecfd63647
Jae-Hong-Kim <newfly101@naver.com>
2024-10-25 11:51:29 +0900

    [검색폼1] [주석] 추가

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -3,9 +3,13 @@ import {qs} from "../helpers.js";
 
 export default class SearchFormView extends View {
     constructor() {
-        super(qs("#search-form-view")); //element id를 갖고온다.
+        super(qs("#search-form-view")); //element id를 기준으로 가져와서 this에 저장
+        // 갖고온 element
+        console.log("this",this);
 
+        // this.element에서 type=reset인 것을 this.resetElement로 랩핑하고 값을 가진다
         this.resetElement = qs("[type=reset]", this.element);
+        // resetElement의 값을 visible option으로 관리해서 보여지고 안보여지고를 구분하게끔 한다.
         this.showResetButton(false);
     }
 
