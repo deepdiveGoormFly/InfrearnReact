6573f8b0370c5438821198b02ce0b85b62f3e5af
Jae-Hong-Kim <newfly101@naver.com>
2024-10-27 03:02:14 +0900

    [최근 검색어1] HistoryListView 만들기

--- 1-vanilla/js/views/HistoryListView.js
+++ 1-vanilla/js/views/HistoryListView.js
@@ -1,5 +1,5 @@
 import KeywordListView from './KeywordListView.js';
-import {qs} from "../helpers.js";
+import {formatRelativeDate, qs} from "../helpers.js";
 
 export default class HistoryListView extends KeywordListView {
     constructor() {
@@ -7,5 +7,28 @@ export default class HistoryListView extends KeywordListView {
     }
 }
 class Template {
+    getList(data = []) {
+        return `
+            <ul class="list">
+                ${data.map(this._getItem).join("")}
+            </ul>
+        `;
+    }
+
+    getEmptyMessage() {
+        return `
+        <div class="empty-box">검색 이력이 없습니다.</div>
+        `;
+    }
+
+    _getItem({id, keyword, date}) {
+        return `
+            <li data-keyword=${keyword}>
+                ${keyword}
+                <span class="date">${formatRelativeDate(date)}</span>
+                <button class="btn-remove"></button>
+            </li>
+        `;
+    }
 
 }
