d6ceb025b2a147a8c10d500af7906060bccfdf65
Jae-Hong-Kim <newfly101@naver.com>
2024-10-30 07:57:00 +0900

    [검색폼1] 상태 갱신하기 - 이벤트 처리 : react에서 이벤트를 처리하는 함수는 handle로 시작한다(관례)

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -6,6 +6,12 @@ class App extends React.Component {
             searchKeyword: "", // 입력값을 나타내는 상태
         };
     }
+
+    handleChangeInput(event) {
+        this.setState({searchKeyword: event.target.value})
+        console.log("searchKeyword : ",this.state.searchKeyword);
+    }
+
     render() {
         return (
             <>
@@ -17,7 +23,9 @@ class App extends React.Component {
                         <input type="text"
                                placeholder="검색어를 입력하세요"
                                autoFocus
-                               value={this.state.searchKeyword}/>
+                               value={this.state.searchKeyword}
+                               onChange={(event) => this.handleChangeInput(event)}
+                        />
                         <button type="reset" className="btn-reset"></button>
                     </form>
                 </div>
@@ -25,19 +33,6 @@ class App extends React.Component {
         );
     }
 }
-// const element = (
-//         <>
-//             <header>
-//                 <h2 className="container">검색</h2>
-//             </header>
-//             <div className="container">
-//                 <form id="search-form-view">
-//                     <input type="text" placeholder="검색어를 입력하세요" autoFocus/>
-//                     <button type="reset" className="btn-reset"></button>
-//                 </form>
-//             </div>
-//         </>
-//     );
 
 ReactDOM.render(<App />, document.querySelector("#app"));
 // ReactDOM.render(element, document.querySelector("#app"));
