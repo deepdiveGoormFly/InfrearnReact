6d685ffe9eefe26c407ad0ba886c74a2bac38612
Jae-Hong-Kim <newfly101@naver.com>
2024-10-30 07:49:45 +0900

    [검색폼1] 리액트 컴포넌트 : <App> 을 이용해서 화면에 그리기

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,22 +1,36 @@
-// 코드가 많아질 겉데, index.html에만 추가하면 코드 관리가 어려워짐
-// main.js는 시작점
-// 코드는 모듈별로 구분해서 저장하기 때문에 import해서 사용할 예정
-// [헤더 구현]
-const element = (
-        <>
-            <header>
-                <h2 className="container">검색</h2>
-            </header>
-            <div className="container">
-                <form id="search-form-view">
-                    <input type="text" placeholder="검색어를 입력하세요" autoFocus/>
-                    <button type="reset" className="btn-reset"></button>
-                </form>
-            </div>
-        </>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <header>
+                    <h2 className="container">검색</h2>
+                </header>
+                <div className="container">
+                    <form id="search-form-view">
+                        <input type="text" placeholder="검색어를 입력하세요" autoFocus/>
+                        <button type="reset" className="btn-reset"></button>
+                    </form>
+                </div>
+            </>
+        );
+    }
+}
+// const element = (
+//         <>
+//             <header>
+//                 <h2 className="container">검색</h2>
+//             </header>
+//             <div className="container">
+//                 <form id="search-form-view">
+//                     <input type="text" placeholder="검색어를 입력하세요" autoFocus/>
+//                     <button type="reset" className="btn-reset"></button>
+//                 </form>
+//             </div>
+//         </>
+//     );
 
-ReactDOM.render(element, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
+// ReactDOM.render(element, document.querySelector("#app"));
 
 // HTML attribute는 소문자만 사용
 // JSX 는 camel case를 사용
