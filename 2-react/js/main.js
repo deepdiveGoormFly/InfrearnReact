61d4226b76991377dde6b51124c251d565357f35
Jae-Hong-Kim <newfly101@naver.com>
2024-10-30 07:52:46 +0900

    [검색폼1] 입력 값을 기억하기 - state : 를 설정하고 inputbox 값 집어넣기

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,4 +1,11 @@
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            searchKeyword: "", // 입력값을 나타내는 상태
+        };
+    }
     render() {
         return (
             <>
@@ -7,7 +14,7 @@ class App extends React.Component {
                 </header>
                 <div className="container">
                     <form id="search-form-view">
-                        <input type="text" placeholder="검색어를 입력하세요" autoFocus/>
+                        <input type="text" placeholder="검색어를 입력하세요" autoFocus value={this.state.searchKeyword}/>
                         <button type="reset" className="btn-reset"></button>
                     </form>
                 </div>
