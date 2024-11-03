5d1a15affa0b20544c7934c2f38d35d2934c27f4
Jae-Hong-Kim <newfly101@naver.com>
2024-11-03 19:48:35 +0900

    [조건부렌더링] 입력값 있을 때 x button 추가

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -13,6 +13,12 @@ class App extends React.Component {
     }
 
     render() {
+        let resetButton = null;
+
+        if (this.state.searchKeyword.length > 0) {
+            resetButton = <button type="reset" className="btn-reset"></button>
+        }
+
         return (
             <>
                 <header>
@@ -26,7 +32,8 @@ class App extends React.Component {
                                value={this.state.searchKeyword}
                                onChange={(event) => this.handleChangeInput(event)}
                         />
-                        <button type="reset" className="btn-reset"></button>
+                        {/*<button type="reset" className="btn-reset"></button>*/}
+                        {resetButton}
                     </form>
                 </div>
             </>
